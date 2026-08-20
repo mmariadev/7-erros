9b868e4ab2fd49855ee859a1379fc9aeb093bd6e
Maria <maria_c_brito@estudante.sesisenai.org.br>
2026-08-20 11:37:01 -0300

    add -  terminei de transcrever o codigo 1

---
+++ code_01.php
@@ -0,0 +1,141 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "crud_aula";
+
+$conn = new mysqli ($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die ("erro na conexão: ". $conn->connect_error); //tinha erro
+}
+
+// CADASTRAR
+
+if (isset($_POST['cadastrar'])){
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO usuarios (nome, email) VALUE (?, ?)";
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param ("ss", $nome, $email);
+    $stmt->execute();
+
+    header("Location: index.php");
+    exit;
+}
+
+
+//EXCLUIR 
+if (isset($_GET['excluir'])){
+    $id = $_GET['excluir'];
+
+$sql = "DELETE FROM usuarios WHERE id = ?";
+$stmt = $conn->prepare($sql);
+
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+header("Location: index.php");
+exit;
+}
+
+//EDITAR
+if(isset($_POST['editar'])){
+    $id = $_POST['id'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+
+    $sql = "UPDATE usuarios SET nome = ?, email = ? WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param("ssi", $nome, $email, $id); //tinha erro 
+    $stmt->execute();
+
+    header("Location: index.php");
+    exit;
+}
+
+//BUSCAR USUÁRIOS
+$sql = "SELECT id, nome, email FROM usuarios ORDER BY id DESC";
+$resultado = $conn->query($sql); //tinha erro
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD de Usuários</title>
+</head>
+
+<body>
+
+    <h1>Cadastro de Usuários</h1>
+
+    <form method="POST">
+
+        <label>nome:</label>
+        <input type="text" name ="nome" required>
+
+        <br><br>
+
+        <label>E-mail:</label>
+        <input type="email" name ="email" required>
+
+        <br><br>
+
+        <button type="submit" name="cadastrar">
+            cadastrar
+        </button>
+
+    </form>
+
+    <h2>Usuários cadastrados</h2>
+
+    <table border="1">
+
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+        </tr>
+
+        <?php while ($usuario = $resultado->fetch_assoc()){ ?>
+
+            <tr>
+
+                <td>
+                    <?=$usuario['id'] ?>
+                </td>
+
+                <td>
+                    <?= $usuario['nome'] ?>
+                </td>
+
+                <td>
+                    <?= $usuario['email'] ?>
+                </td>
+
+                <td>
+                    <a href="idex.php?exluir=<?= $usuario ['id'] ?>">
+                        excluir
+                    </a>
+                </td>
+
+            </tr>
+            
+        <?php } ?> // erro
+    
+    </table>
+
+</body>
+
+</html>
+        
+
+        }
